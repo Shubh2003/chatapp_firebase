7bc43e7eb1c8274fca7b031a290507d00f8b320a
Shubham Daulkar <daulkarshubham@gmail.com>
2023-03-01 23:06:40 +0530

    first commit

---
+++ chatapp_firebase/lib/shared/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static String appId = "1:34580616621:web:2b9470a88e253e272a3d24";
+  static String apiKey = "AIzaSyCKHRadGu9PYuJnsdwFxrEfmt62fEicHfg";
+  static String messagingSenderId = "34580616621 ";
+  static String projectId = "chatappflutter-a98f0";
+
+
+}
